6b0c20b9800c76ede254623ac27ac5545b584e97
Argha <argha@intellocut.com>
2017-02-04 01:15:44 +0530

    add meaningful variable name

--- lib/minesweeper/bomb.rb
+++ lib/minesweeper/bomb.rb
@@ -1,9 +1,9 @@
 module Minesweeper
   class Bomb
-    def self.place_mines(board, mine_count, fix_init = [0,0])
+    def self.place_mines(board, mine_count, safe_begining_pos = [0,0])
       mines = mine_count
       temp_board = board.flatten
-      fix_position = fix_init[0] * board[0].length + fix_init[1]
+      fix_position = safe_begining_pos[0] * board[0].length + safe_begining_pos[1]
       book_keep = Hash.new(0)
       while temp_board.count('x') != mine_count
         rand_index = rand(temp_board.length)
