95998e8ea9da2783d2f79961214257a572b11018
Argha <rghchaks73@gmail.com>
2017-02-05 19:40:35 +0530

    add some thor features

--- lib/minesweeper/cli.rb
+++ lib/minesweeper/cli.rb
@@ -2,9 +2,20 @@ require 'thor'
 
 module Minesweeper
   class Cli < Thor
+
+
     desc "play", "Player can start playing"
     def play
-      puts Minesweeper::GameRunner.prompt_level
+      say(Minesweeper::GameRunner.prompt_level)
+      prompt_levels if @no_of_prompts == 0
+      level = ask("Choose Level :")
+      begin 
+        !!Integer(level)
+        system('clear')
+      rescue ArgumentError
+        say("Why Kidding?")
+        play
+      end
     end
   end
 end
